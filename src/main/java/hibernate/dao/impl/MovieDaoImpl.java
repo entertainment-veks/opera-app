0652eb3a8dba08858877129fc30d4e7a5ba6752f
egorka <entertainment.veks@gmail.com>
2021-01-27 15:41:33 +0200

    code refactored following Sofa's recomendations

--- src/main/java/hibernate/dao/impl/MovieDaoImpl.java
+++ src/main/java/hibernate/dao/impl/MovieDaoImpl.java
@@ -15,7 +15,9 @@ public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
         Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
             Long itemId = (Long) session.save(movie);
             transaction.commit();
@@ -26,6 +28,8 @@ public class MovieDaoImpl implements MovieDao {
                 transaction.rollback();
             }
             throw new CustomDaoException("Can't insert Movie entity", e);
+        } finally {
+            session.close();
         }
     }
 
