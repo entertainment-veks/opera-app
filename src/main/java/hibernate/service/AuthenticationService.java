69ff788278e3a0f03f301176fa384e3fb4c7d2cf
egorka <entertainment.veks@gmail.com>
2021-02-03 12:35:20 +0200

    refactored following Maksym's recomendations

--- src/main/java/hibernate/service/AuthenticationService.java
+++ src/main/java/hibernate/service/AuthenticationService.java
@@ -6,11 +6,5 @@ import hibernate.model.User;
 public interface AuthenticationService {
     User login(String email, String password) throws AuthenticationException;
 
-    /**
-     * We should register a new user. New user entity will contains the email and password
-     * @param email - user email. should be unique for each user
-     * @param password - user password
-     * @return new user instance
-     */
     User register(String email, String password);
 }
