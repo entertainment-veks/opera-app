0c123e32ba553900b4966e18bcf4e49f7213881b
egorka <entertainment.veks@gmail.com>
2021-02-22 14:58:56 +0200

    refactored following recoms

--- src/main/java/hibernate/controller/InjectDataController.java
+++ src/main/java/hibernate/controller/InjectDataController.java
@@ -9,12 +9,10 @@ import hibernate.service.RoleService;
 import hibernate.service.UserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/inject-data")
+@Component
 public class InjectDataController {
     private UserService userService;
     private RoleService roleService;
